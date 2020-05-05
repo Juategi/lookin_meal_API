3adf48184ba827662839d9cc2e89d85d4b7e7104
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-05-05 17:49:05 +0200

    little fix

--- functions/restaurants.js
+++ functions/restaurants.js
@@ -31,7 +31,7 @@ const createRestaurant = (request, response) => {
   const addMenuEntry = (request, response) => {
     const {restaurant_id, name, section, price} = request.body
     pool.query('INSERT INTO menuentry (restaurant_id, name, section, price) VALUES ($1, $2, $3, $4)',
-     [restaurant_id, name, section, rating, numReviews, price], (error, results) => {
+     [restaurant_id, name, section, price], (error, results) => {
       if (error) {
         throw error
       }
