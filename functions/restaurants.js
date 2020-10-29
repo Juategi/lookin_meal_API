33c8eedc985a3b2646f0c0a12f74403d0f5292f0
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-10-29 14:20:16 +0100

    m

--- functions/restaurants.js
+++ functions/restaurants.js
@@ -1,14 +1,26 @@
 const pool = require("./mypool").pool
 
 const createRestaurant = (request, response) => {
-    const {taid, name, phone, website, webUrl, address, email, city, country, latitude, longitude,rating, numrevta, images, types, schedule, sections, currency} = request.body
-    pool.query('INSERT INTO restaurant (ta_id, name, phone, website, webUrl, address, email, city, country, latitude, longitude, rating, numrevta, images, types, schedule, sections, currency) VALUES ($1, $2, $3, $4, $5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18)',
-     [taid, name, phone, website, webUrl, address, email, city, country, latitude, longitude,rating, numrevta, images, types, schedule, sections, currency], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`Restaurant added with name: ${name}`)
-    })
+    const {taid, name, phone, website, webUrl, address, email, city, country, latitude, longitude,rating, numrevta, images, types, schedule, sections, currency, delivery} = request.body
+    if(taid == -1){
+      pool.query('INSERT INTO restaurant (name, phone, website, webUrl, address, email, city, country, latitude, longitude, rating, numrevta, images, types, schedule, sections, currency, delivery) VALUES ($1, $2, $3, $4, $5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18) RETURNING restaurant_id',
+      [name, phone, website, webUrl, address, email, city, country, latitude, longitude,rating, numrevta, images, types, schedule, sections, currency, delivery], (error, results) => {
+       if (error) {
+         throw error
+       }
+       response.status(200).json(results.rows)
+     })
+    }
+    else{
+      pool.query('INSERT INTO restaurant (ta_id, name, phone, website, webUrl, address, email, city, country, latitude, longitude, rating, numrevta, images, types, schedule, sections, currency, delivery) VALUES ($1, $2, $3, $4, $5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19) RETURNING restaurant_id',
+      [taid, name, phone, website, webUrl, address, email, city, country, latitude, longitude,rating, numrevta, images, types, schedule, sections, currency, delivery], (error, results) => {
+       if (error) {
+         throw error
+       }
+       response.status(200).json(results.rows)
+     })
+    }
+    
   }
 
   const getRestaurants = (request, response) => {
