7b2f4131179a07c840cedb88456cda495d649faa
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-11-06 23:00:16 +0100

    no text

--- functions/search.js
+++ functions/search.js
@@ -10,6 +10,26 @@ const queryRestaurants = (request, response) => {
     console.log(valoration)
     console.log(offset)
     console.log(types)
+
+    if(valoration != "Sort by relevance"){
+        statement = "select *, distance($1, $2, latitude, longitude) as distance from restaurant where distance($1, $2, latitude, longitude) <= $3 and to_tsvector('simple', name) @@ to_tsquery('simple', $5) and types && $6::text[] order by distance asc limit 10 offset $4 rows;"
+    }
+    else{
+        statement = "select r.*, distance($1, $2, r.latitude, r.longitude) as distance, COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) from restaurant r LEFT OUTER join menuentry m on m.restaurant_id = r.restaurant_id LEFT OUTER join rating ra on ra.entry_id = m.entry_id where distance($1, $2, r.latitude, r.longitude) <= $3  and r.types && $6::text[] and to_tsvector('simple', r.name) @@ to_tsquery('simple', $5) group by r.restaurant_id order by COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) desc limit 10 offset $4 rows;"
+    }
+    if(types == "{}"){
+      statement = statement.replace("and r.types && $6::text[]", "and $6=$6")
+    }
+    if(query == ":*"){
+      statement = statement.replace("and to_tsvector('simple', r.name) @@ to_tsquery('simple', $5)", "and $5=$5")
+    }
+    pool.query(statement,[latitude, longitude, distance, offset,query, types], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+    /*
     if(valoration != "Sort by relevance"){
       if(types == "{}"){
         console.log('aaaaa')
@@ -23,11 +43,11 @@ const queryRestaurants = (request, response) => {
     else{
       if(types == "{}"){
         console.log('iiii')
-        statement = "select r.name, distance($2, $3, r.latitude, r.longitude) as distance,  COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) from restaurant r LEFT OUTER join menuentry m on m.restaurant_id = r.restaurant_id LEFT OUTER join rating ra on ra.entry_id = m.entry_id where distance($2, $3, r.latitude, r.longitude) <= $4  and to_tsvector('simple', r.name) @@ to_tsquery('simple', $1) group by r.restaurant_id order by COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) desc limit 10 offset $5 rows;"
+        statement = "select r.*, distance($2, $3, r.latitude, r.longitude) as distance,  COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) from restaurant r LEFT OUTER join menuentry m on m.restaurant_id = r.restaurant_id LEFT OUTER join rating ra on ra.entry_id = m.entry_id where distance($2, $3, r.latitude, r.longitude) <= $4  and to_tsvector('simple', r.name) @@ to_tsquery('simple', $1) group by r.restaurant_id order by COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) desc limit 10 offset $5 rows;"
       }
       else{
         console.log('oooo')
-        statement = "select r.name, distance($2, $3, r.latitude, r.longitude) as distance, COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) from restaurant r LEFT OUTER join menuentry m on m.restaurant_id = r.restaurant_id LEFT OUTER join rating ra on ra.entry_id = m.entry_id where distance($2, $3, r.latitude, r.longitude) <= $4  and r.types && $6::text[] and to_tsvector('simple', r.name) @@ to_tsquery('simple', $1) group by r.restaurant_id order by COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) desc limit 10 offset $5 rows;"
+        statement = "select r.*, distance($2, $3, r.latitude, r.longitude) as distance, COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) from restaurant r LEFT OUTER join menuentry m on m.restaurant_id = r.restaurant_id LEFT OUTER join rating ra on ra.entry_id = m.entry_id where distance($2, $3, r.latitude, r.longitude) <= $4  and r.types && $6::text[] and to_tsvector('simple', r.name) @@ to_tsquery('simple', $1) group by r.restaurant_id order by COALESCE((sum(ra.rating)*0.5/count(ra) + count(ra)*0.0025),0 ) desc limit 10 offset $5 rows;"
       }
     }
     if(types == "{}"){
@@ -46,7 +66,7 @@ const queryRestaurants = (request, response) => {
         response.status(200).json(results.rows)
       })
     }
-    
+    */
   }
 
   
