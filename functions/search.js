703796d3dd930bc283e1d1e441d3b6243f3ea01e
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-12-19 14:17:23 +0100

    FIXINGS

--- functions/search.js
+++ functions/search.js
@@ -90,128 +90,124 @@ const queryEntries = (request, response) => {
     console.log(allergens1)
     console.log(allergens2)
     console.log(allergens3)
-    if(rating1 == "0.0"){
-      rating1 = ""
-    }
-    if(rating2 == "0.0"){
-      rating2 = ""
-    }
-    if(rating3 == "0.0"){
-      rating3 = ""
-    }
-    if(price1 == "0.0"){
-      price1 = ""
-    }
-    if(price2 == "0.0"){
-      price2 = ""
-    }
-    if(price3 == "0.0"){
-      price3 = ""
-    }
+    
   if(query3 != ""){
-    var statement = "select re.*, e.entry_id as id1, w.entry_id as id2, x.entry_id as id3, distance($1, $2, re.latitude, re.longitude) as distance from menuentry e left join restaurant re on re.restaurant_id = e.restaurant_id left join menuentry w on w.restaurant_id = e.restaurant_id left join menuentry x on x.restaurant_id = e.restaurant_id left join rating r on r.entry_id = e.entry_id left join rating v on v.entry_id = w.entry_id left join rating y on y.entry_id = x.entry_id where $6=$6 and $7=$7 and $8=$8 and to_tsvector('simple', e.name) @@ to_tsquery('simple', $3) and to_tsvector('simple', w.name) @@ to_tsquery('simple', $4) and to_tsvector('simple', x.name) @@ to_tsquery('simple', $5) and (e.allergens && $12::text[] == False) and (w.allergens && $13::text[] == False) and (x.allergens && $14::text[] == False) and e.price <= $9 and w.price <= $10 and x.price <= $11 group by e.entry_id, re.restaurant_id, w.entry_id, x.entry_id having AVG(r.rating) > $6 and AVG(v.rating) > $7 and AVG(y.rating) > $8 order by distance($1, $2, re.latitude, re.longitude) asc limit 12;"
+    var statement = "select re.*, e.entry_id as id1, w.entry_id as id2, x.entry_id as id3, distance($1, $2, re.latitude, re.longitude) as distance from menuentry e left join restaurant re on re.restaurant_id = e.restaurant_id left join menuentry w on w.restaurant_id = e.restaurant_id left join menuentry x on x.restaurant_id = e.restaurant_id left join rating r on r.entry_id = e.entry_id left join rating v on v.entry_id = w.entry_id left join rating y on y.entry_id = x.entry_id where $6=$6 and $7=$7 and $8=$8 and to_tsvector('simple', e.name) @@ to_tsquery('simple', $3) and to_tsvector('simple', w.name) @@ to_tsquery('simple', $4) and to_tsvector('simple', x.name) @@ to_tsquery('simple', $5) and (e.allergens && $12::text[] = False) and (w.allergens && $13::text[] = False) and (x.allergens && $14::text[] = False) and e.price <= $9 and w.price <= $10 and x.price <= $11 group by e.entry_id, re.restaurant_id, w.entry_id, x.entry_id having AVG(r.rating) > $6::real and AVG(v.rating) > $7::real and AVG(y.rating) > $8::real order by distance($1, $2, re.latitude, re.longitude) asc limit 12;"
 
     if(valoration == "Sort by price lower first"){
-      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by (e.price + w.price + x.price) asc")
+      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by case when (e.price + w.price + x.price) != 0 then (e.price + w.price + x.price) end asc, case when (e.price + w.price + x.price) = 0 then (e.price + w.price + x.price) end desc")
     }
     else if(valoration == "Sort by relevance"){
-      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by (sum(r.rating) + sum(v.rating) + sum(y.rating)) desc")
+      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by (count(r) + count(v)  + count(y)) desc")
     }
 
-    if(rating1 == "" && rating2 == "" && rating3 == ""){
-      statement = statement.replace("having AVG(r.rating) > $6 and AVG(v.rating) > $7 and AVG(y.rating) > $8", "")
+    if(rating1 == "0.0" && rating2 == "0.0" && rating3 == "0.0"){
+      statement = statement.replace("having AVG(r.rating) > $6::real and AVG(v.rating) > $7::real and AVG(y.rating) > $8::real", "")
     }
-    if(rating1 == ""){
-      statement = statement.replace("AVG(r.rating) > $6 and", "")
+    if(rating1 == "0.0"){
+      statement = statement.replace("AVG(r.rating) > $6::real and", "")
     }
-    if(rating2 == ""){
-      statement = statement.replace("AVG(v.rating) > $7 and", "")
+    if(rating2 == "0.0"){
+      statement = statement.replace("AVG(v.rating) > $7::real and", "")
     }
-    if(rating3 == ""){
-      statement = statement.replace("AVG(y.rating) > $8", "")
+    if(rating3 == "0.0"){
+      statement = statement.replace("AVG(y.rating) > $8::real", "")
     }
 
-    if(price1 == ""){
+    if(price1 == "0.0"){
       statement = statement.replace("e.price <= $9 and", "$9=$9 and ")
     }
-    if(price2 == ""){
+    if(price2 == "0.0"){
       statement = statement.replace("w.price <= $10 and", "$10=$10 and ")
     }
-    if(price3 == ""){
+    if(price3 == "0.0"){
       statement = statement.replace("and x.price <= $11", " and $11=$11")
     }
 
     if(allergens1 == "{}"){
-      statement = statement.replace("(e.allergens && $12::text[] == False) and", "$12=$12 and ")
+      statement = statement.replace("(e.allergens && $12::text[] = False) and", "$12=$12 and ")
     }
     if(allergens2 == "{}"){
-      statement = statement.replace("(w.allergens && $12::text[] == False) and", "$13=$13 and ")
+      statement = statement.replace("(w.allergens && $12::text[] = False) and", "$13=$13 and ")
     }
     if(allergens3 == "{}"){
-      statement = statement.replace("(x.allergens && $12::text[] == False) and", "$14=$14 and ")
+      statement = statement.replace("(x.allergens && $12::text[] = False) and", "$14=$14 and ")
     }
   }
   else if(query2 != ""){
-    var statement = "select re.*, e.entry_id as id1, w.entry_id as id2, distance($1, $2, re.latitude, re.longitude) as distance from menuentry e left join restaurant re on re.restaurant_id = e.restaurant_id left join menuentry w on w.restaurant_id = e.restaurant_id left join rating r on r.entry_id = e.entry_id left join rating v on v.entry_id = w.entry_id where $6=$6 and $7=$7 and to_tsvector('simple', e.name) @@ to_tsquery('simple', $3) and to_tsvector('simple', w.name) @@ to_tsquery('simple', $4) and (e.allergens && $12::text[] == False) and (w.allergens && $13::text[] == False) and e.price <= $9 and w.price <= $10 and ($5=$5 and $8=$8 and $11=$11 and $14=$14) group by e.entry_id, re.restaurant_id, w.entry_id having AVG(r.rating) > $6 and AVG(v.rating) > $7 order by distance($1, $2, re.latitude, re.longitude) asc limit 12;"
+    var statement = "select re.*, e.entry_id as id1, w.entry_id as id2, distance($1, $2, re.latitude, re.longitude) as distance from menuentry e left join restaurant re on re.restaurant_id = e.restaurant_id left join menuentry w on w.restaurant_id = e.restaurant_id left join rating r on r.entry_id = e.entry_id left join rating v on v.entry_id = w.entry_id where $6=$6 and $7=$7 and to_tsvector('simple', e.name) @@ to_tsquery('simple', $3) and to_tsvector('simple', w.name) @@ to_tsquery('simple', $4) and (e.allergens && $12::text[] = False) and (w.allergens && $13::text[] = False) and e.price <= $9 and w.price <= $10 and ($5=$5 and $8=$8 and $11=$11 and $14=$14) group by e.entry_id, re.restaurant_id, w.entry_id having AVG(r.rating) > $6::real and AVG(v.rating) > $7::real order by distance($1, $2, re.latitude, re.longitude) asc limit 12;"
 
     if(valoration == "Sort by price lower first"){
-      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by (e.price + w.price) asc")
+      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by case when (e.price + w.price) != 0 then (e.price + w.price) end asc, case when (e.price + w.price) = 0 then (e.price + w.price) end desc")
     }
     else if(valoration == "Sort by relevance"){
-      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by (sum(r.rating) + sum(v.rating)) desc")
+      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by (count(r) + count(v)) desc")
     } 
 
-    if(rating1 == "" && rating2 == ""){
-      statement = statement.replace("having AVG(r.rating) > $6 and AVG(v.rating) > $7 ", "")
+    if(rating1 == "0.0" && rating2 == "0.0"){
+      statement = statement.replace("having AVG(r.rating) > $6::real and AVG(v.rating) > $7::real ", "")
     }
-    if(rating1 == ""){
-      statement = statement.replace("AVG(r.rating) > $6 and", "")
+    if(rating1 == "0.0"){
+      statement = statement.replace("AVG(r.rating) > $6::real and", "")
     }
-    if(rating2 == ""){
-      statement = statement.replace("AVG(v.rating) > $7", "")
+    if(rating2 == "0.0"){
+      statement = statement.replace("AVG(v.rating) > $7::real", "")
     }
 
-    if(price1 == ""){
+    if(price1 == "0.0"){
       statement = statement.replace("e.price <= $9 and", "$9=$9 and ")
     }
-    if(price2 == ""){
+    if(price2 == "0.0"){
       statement = statement.replace("w.price <= $10 and", "$10=$10 and ")
     }
 
     if(allergens1 == "{}"){
-      statement = statement.replace("(e.allergens && $12::text[] == False) and", "$12=$12 and ")
+      statement = statement.replace("(e.allergens && $12::text[] = False) and", "$12=$12 and ")
     }
     if(allergens2 == "{}"){
-      statement = statement.replace("(w.allergens && $12::text[] == False) and", "$13=$13 and ")
+      statement = statement.replace("(w.allergens && $12::text[] = False) and", "$13=$13 and ")
     }
   }
   else{
-    var statement = "select re.*, e.entry_id as id1, distance($1, $2, re.latitude, re.longitude) as distance from menuentry e left join restaurant re on re.restaurant_id = e.restaurant_id left join rating r on r.entry_id = e.entry_id where $6=$6 and to_tsvector('simple', e.name) @@ to_tsquery('simple', $3) and (e.allergens && $12::text[] == False) and e.price <= $9 and ($4=$4 and $5=$5 and $7=$7 and $8=$8 and $10=$10 and $11=$11 and $13=$13 and $14=$14) group by e.entry_id, re.restaurant_id having AVG(r.rating) > $6 order by distance($1, $2, re.latitude, re.longitude) asc limit 12;"
+    var statement = "select re.*, e.entry_id as id1, distance($1, $2, re.latitude, re.longitude) as distance from menuentry e left join restaurant re on re.restaurant_id = e.restaurant_id left join rating r on r.entry_id = e.entry_id where $6=$6 and to_tsvector('simple', e.name) @@ to_tsquery('simple', $3) and (e.allergens && $12::text[] = False) and e.price <= $9 and ($4=$4 and $5=$5 and $7=$7 and $8=$8 and $10=$10 and $11=$11 and $13=$13 and $14=$14) group by e.entry_id, re.restaurant_id having AVG(r.rating) > $6::real order by distance($1, $2, re.latitude, re.longitude) asc limit 12;"
 
     if(valoration == "Sort by price lower first"){
-      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by e.price asc")
+      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by case when e.price != 0 then e.price end asc, case when e.price = 0 then e.price end desc")
     }
     else if(valoration == "Sort by relevance"){
-      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by sum(r.rating) desc")
+      statement = statement.replace("order by distance($1, $2, re.latitude, re.longitude) asc", "order by count(r) desc")
     } 
 
-    if(rating1 == ""){
-      statement = statement.replace("having AVG(r.rating) > $6", "")
+    if(rating1 == "0.0"){
+      statement = statement.replace("having AVG(r.rating) > $6::real", "")
     }
-    if(price1 == ""){
+    if(price1 == "0.0"){
       statement = statement.replace("e.price <= $9 and", "$9=$9 and ")
     }
     if(allergens1 == "{}"){
-      statement = statement.replace("(e.allergens && $12::text[] == False) and", "$12=$12 and ")
+      statement = statement.replace("(e.allergens && $12::text[] = False) and", "$12=$12 and ")
     }
   }
 
+  var result;
+  stop_words = ['a', 'al', 'con', 'de', 'del', 'e', 'el', 'en', 'la', 'las', 'lo', 'los', 'y', 'an', 'and', 'the', 'of', 'with']
   pool.query(statement,[latitude, longitude, query1, query2, query3, rating1 , rating2 , rating3 , price1, price2, price3, allergens1, allergens2, allergens3], (error, results) => {
     if (error) {
       throw error
     }
+    result = results.rows[1]
+    var count = 0;
+    for(var i in result) {
+        count++;
+    }
+    console.log(count)
+    if(count < 12){
+      var queryVector1 = query1.split(" ")
+      console.log(queryVector1)
+    }
     response.status(200).json(results.rows)
   })
+  
 }
 
 
