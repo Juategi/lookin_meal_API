7c9b478d974f63dbf659f6044eb1aea597e221d3
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2021-04-09 14:33:42 +0200

    statistics

--- functions/statistics.js
+++ functions/statistics.js
@@ -3,7 +3,7 @@ const pool = require("./mypool").pool
 const addVisit = (request, response) => {
     const {user_id, restaurant_id} = request.body
     pool.query(
-      'INSERT INTO visits (user_id, restaurant_id, visit) VALUES ($1, $2, CURRENT_DATE)',
+      'INSERT INTO visits (user_id, restaurant_id, visit) VALUES ($1, $2, CURRENT_TIMESTAMP)',
       [user_id, restaurant_id],
       (error, results) => {
         if (error) {
@@ -16,7 +16,7 @@ const addVisit = (request, response) => {
 
 const getVisits = (request, response) => {
     const {restaurant_id} = request.headers;
-    pool.query("SELECT COUNT(*) as visits FROM visits where restaurant_id = $1", [restaurant_id], (error, results) => {
+    pool.query("SELECT * FROM visits where restaurant_id = $1", [restaurant_id], (error, results) => {
       if (error) {
         throw error
       }
@@ -28,20 +28,20 @@ const getVisits = (request, response) => {
 const addRate = (request, response) => {
     const {user_id, restaurant_id, entry_id, withcomment} = request.body
     pool.query(
-      'INSERT INTO rates (user_id, restaurant_id, entry_id, withcomment, rate) VALUES ($1, $2, $3, $4, CURRENT_DATE)',
+      'INSERT INTO rates (user_id, restaurant_id, entry_id, withcomment, rate) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP)',
       [user_id, restaurant_id, entry_id, withcomment],
       (error, results) => {
         if (error) {
           throw error
         }
-        response.status(201).send(`visited: ${restaurant_id}`)
+        response.status(201).send(`rated: ${restaurant_id}`)
       }
   )
 }
 
 const getRate = (request, response) => {
     const {restaurant_id} = request.headers;
-    pool.query("(SELECT COUNT(*) as nocomment FROM rates where restaurant_id = $1 and withcomment=false) UNION ALL (SELECT COUNT(*) as withcomment FROM rates where restaurant_id = $1 and withcomment=true)", [restaurant_id], (error, results) => {
+    pool.query("(SELECT * FROM rates where restaurant_id = $1) ", [restaurant_id], (error, results) => {
       if (error) {
         throw error
       }
